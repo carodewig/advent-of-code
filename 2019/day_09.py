fb4be779c66118a7f5843a2fabf5dffb35426237
Caroline <carodewig@mac.com>
2020-01-28 17:44:11 -0500

    WSP corrections

--- 2019/day_09.py
+++ 2019/day_09.py
@@ -6,8 +6,9 @@ https://adventofcode.com/2019/day/9
 from day_05 import IntcodeComputer, test_intcode_computer
 
 #IntcodeComputer([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]).parse()
-assert len(str(IntcodeComputer([1102,34915192,34915192,7,4,7,99,0], return_rather_than_print=True).parse())) == 16
-assert IntcodeComputer([104,1125899906842624,99], return_rather_than_print=True).parse() == 1125899906842624
+test_intcode_computer()
+assert len(str(IntcodeComputer([1102, 34915192, 34915192, 7, 4, 7, 99, 0], return_rather_than_print=True).parse())) == 16
+assert IntcodeComputer([104, 1125899906842624, 99], return_rather_than_print=True).parse() == 1125899906842624
 
 BOOST = IntcodeComputer.init_from_file("data/09.txt", replace_reads_value=[2])
 BOOST.parse()
