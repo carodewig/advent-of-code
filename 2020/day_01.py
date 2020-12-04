08f88bf9fee0ecfd782322cd1cafa32e201f6b58
carodewig <carodewig@mac.com>
2020-12-03 18:52:53 -0600

    It's 2020!

---
+++ 2020/day_01.py
@@ -0,0 +1,56 @@
+
+from math import floor
+
+
+def two_entries_that_sum_to(goal, entries):
+    if len(entries) < 2:
+        return None
+
+    entries_sorted = sorted(entries)
+
+    index1 = 0
+    index2 = len(entries_sorted) - 1
+
+    while index1 < index2:
+        result = entries_sorted[index1] + entries_sorted[index2]
+
+        if result == goal:
+            return entries_sorted[index1] * entries_sorted[index2]
+
+        if result < goal:
+            index1 += 1
+        else:
+            index2 -= 1
+
+    return None
+
+
+def three_entries_that_sum_to(goal, entries):
+    entries_sorted = sorted(entries)
+
+    if len(entries) < 3:
+        return None
+
+    for index in range(len(entries)):
+        value = entries_sorted[index]
+        if partial_result := two_entries_that_sum_to(goal - value, entries_sorted[index + 1:]):
+            return partial_result * value
+
+    return None
+
+
+def load_entries_from_text(filename):
+    entries = []
+
+    with open(filename) as fh:
+        for line in fh:
+            entries.append(int(line.strip()))
+
+    return entries
+
+
+assert two_entries_that_sum_to(2020, [1721, 979, 366, 299, 675, 1456]) == 514579
+assert two_entries_that_sum_to(2020, load_entries_from_text("data/01.txt")) == 866436
+
+assert three_entries_that_sum_to(2020, [1721, 979, 366, 299, 675, 1456]) == 241861950
+assert three_entries_that_sum_to(2020, load_entries_from_text("data/01.txt")) == 276650720
